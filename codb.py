2cd3566007a18d2e4dccbd15bf4ce1ba8409881b
Song Wang <songwang0328@gmail.com>
2019-05-24 00:00:36 -0400

    updated codb with table name correction feature

--- codb.py
+++ codb.py
@@ -36,9 +36,29 @@ def rollback():
     c.commit()
 
 def upload_table(tblname, df, co):
-    create_tbl(tblname, df.columns.tolist(), co)
+    # name correction
+    df_cols = []
+    for x in df.columns:
+        if x[0].isdigit():
+            newx = input("correction: {} starting with a number\n".format(x))
+            while newx[0].isdigit() or len(newx) > 30:
+                newx = input("correction: {} starting with a number".format(x))
+            df_cols += [newx]
+            continue
+        if len(x) > 30:
+            newx = input("correction: {} too long\n".format(x))
+            while len(newx) > 30:
+                newx = input("correction: {} too long".format(x))
+            df_cols += [newx]
+            continue
+        df_cols += [x]
     
+    # create table template
+    create_tbl(tblname, df_cols, co)
+    
+    # insert into database
     sql = """INSERT INTO {} VALUES """.format(tblname)
+    df.columns = df_cols
     todo = df.astype(str).copy()
     length = todo.shape[0]
     incremental = 0
@@ -58,8 +78,10 @@ def upload_table(tblname, df, co):
                      ', '.join(val) +
                      ")\n")
         if (incremental > 999) or (length - (row+1) == 0):
-            co.cursor().execute(data)
+            try:
+                co.cursor().execute(data)
+            except:
+                rollback()
             incremental = 0
             data = sql
     co.commit()
-            
