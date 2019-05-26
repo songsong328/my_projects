def80855b9c609c766de985ae7f54ce7d2427395
Song Wang <songwang0328@gmail.com>
2019-05-25 23:02:16 -0400

    added summoners name and last 10 games search result

--- main.py
+++ main.py
@@ -6,23 +6,54 @@ Created on Thu May 23 22:23:01 2019
 """
 
 import pandas as pd
-import numpy as np
-import requests
+#import numpy as np
 import urllib
-from songs_tool import codb
-#co = codb.conndb()
+import json
 
-url = "https://na.op.gg/summoner/userName=NoFaith"
-headers = {'User-Agent': """Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36""",
-           'Host': "na.op.gg"}
+from bs4 import BeautifulSoup
+from songs_tool import codb
+
+co = codb.conndb()
+
+host = "na.op.gg"
+header_qry = "SELECT * FROM MYHEADERS"
+
+headers = {'User-Agent': pd.read_sql(header_qry, co).iloc[0][0],
+           'Host': host}
 
 parser = {'name': 'English'}
-
 data = bytes(urllib.parse.urlencode(parser), encoding='utf-8')
-opgg = urllib.request.Request(url=url, data=data, headers=headers, method='POST')
-response = urllib.request.urlopen(quora)
 
+# get all summoners name
+for i in range(100):
+    url = "https://na.op.gg/ranking/ladder/page={}".format(i+1)
+    opgg = urllib.request.Request(url=url,
+                                  data=data,
+                                  headers=headers, method='POST')
+    response = urllib.request.urlopen(opgg)
+    # parse the output
 
-print(response.read().decode('utf-8'))
-
-
+    soup = BeautifulSoup(response.read().decode('utf-8'))
+    
+    # 1 --> summoner name
+    # 2 --> tier
+    # 3 --> lp
+    # 4 --> level
+    # 5 --> winratio
+    
+    this_name = soup.find_all('td')[i+7].text
+    this_tier = soup.find_all('td')[i+8].text.replace('\t', '').replace('\n', '')
+    this_winrate = soup.find_all('td')[i+11].text[-3:]
+    
+    # search this summoner
+    
+    url = "https://na.op.gg/summoner/userName={}".format(this_name)
+    opgg = urllib.request.Request(url=url,
+                                  data=data,
+                                  headers=headers, method='POST')
+    
+    one = ':'.join(soup.find_all('script')[43].text.split(',')[7].split(':')[1:])
+    two = ', '.join(soup.find_all('script')[43].text.split(',')[8:-15])
+    games = one[1:] + ',' + two[:-1]
+    games = games[:games.index('championsList')-4]
+    json.loads(games)
